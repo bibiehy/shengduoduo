6b07f247f0a0f931b6777e260778d66858ce92c3
zhanwei <zhanwei@dianhun.cn>
2024-10-15 18:28:27 +0800

    代码提交

--- pages/user_fahuoren/components/main/main.js
+++ pages/user_fahuoren/components/main/main.js
@@ -1,6 +1,7 @@
 
 import useRequest from '../../../../utils/request';
 import { fetchGetGlobalInfo } from '../../../../service/global';
+import { fetchMainList } from '../../../../service/user_fahuoren';
 
 // 根据指定日期返回与当前日期的差值
 function compareDate(dateStr) {
@@ -29,9 +30,20 @@ Component({
 
 	},
 	data: {
-		opacity: 0,
+        // navigation-bar
+        opacity: 0,
+        // banner
 		swiperNavigation: { type: 'dots-bar' }, //  navigation="{{swiperNavigation}}"
-		swiperList: [], // banner
+        swiperList: [],
+        // 最近创建的任务
+        taskList: [],
+        // 打印标签弹窗
+        showPrintLabel: false,
+        confirmBtn: { content: '确定', variant: 'base' },
+        // 取消任务
+        showCancelTask: false,
+        taskStyle: { minHeight: '120px' },
+        cancelReason: '', // 取消原因
 	},
 	methods: {
 		// 控制透明度 navigation-bar
@@ -48,13 +60,40 @@ Component({
 				const fileList = result['banner'];
 				this.setData({ swiperList: fileList });
 			}
-		}
+        },
+        // 最近创建的任务
+        async getLastestTask() {
+            const result = await useRequest(() => fetchMainList());
+			if(result) {
+				this.setData({ taskList: result });
+			}
+        },
+        // 打印标签弹窗
+        onSurePrint() {
+            this.setData({ showPrintLabel: false });
+        },
+        onCancelPrint() {
+            this.setData({ showPrintLabel: false });
+        },
+        // 取消任务弹窗
+        onSureCancel() {
+            this.setData({ showCancelTask: false });
+        },
+        onCancelTask() {
+            this.setData({ showCancelTask: false });
+        },
+        onChangeTextarea(e) {
+            const textValue = (e.detail.value).replace(/(^\s+)|(\s+$)/g, '');
+            this.setData({ cancelReason: textValue });
+        }
 	},
 	// 自定义组件内的生命周期
     lifetimes: {
 		attached() { // 组件完全初始化完毕
 			// 获取banner
-			this.getBannerList();
+            this.getBannerList();
+            // 最近创建的任务
+            this.getLastestTask();
         },
         detached() { // 组件实例被从页面节点树移除时执行
 
---
+++ service/user_fahuoren.js
@@ -0,0 +1,2 @@
+/************************** 主页 ***********************/
+export const fetchMainList = (params) => ({ url: '/consignor/getWeekTask', method: 'get', loading: true });
